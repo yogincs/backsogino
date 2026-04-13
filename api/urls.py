c759b8580dc79725e28949b600023b058d9672ba
YOGIN CS <shettysumithshetty7@gmail.com>
2026-04-13 19:17:52 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from .views import email, SuggestionCreateView
 urlpatterns = [
     path('email/', email),
     path('suggestion/', SuggestionCreateView.as_view()),
+    path('list/', list_data),
+    path('submit/', submit_data),
 ]
